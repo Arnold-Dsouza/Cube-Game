7ba690020096e0fff0c22cbf00aa6b6a0570d576
Arnold-Dsouza <arnolddsouza1999@gmail.com>
2025-05-13 20:10:06 +0200

    Refactor code structure for improved readability and maintainability

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'the-cube/dist')));
+
+// For any request, send the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'the-cube/dist/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Press Ctrl+C to stop the server`);
+});
